8759713311db1c207a80dea3c41e2e1460a99078
Max Yi-Hsun Chou <maxchou415@gmail.com>
2019-03-02 09:28:35 +0800

    - Add Footer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
+    <Footer />
   </div>
 </template>
+
+<script>
+import Footer from '@/components/Footer'
+
+export default {
+  components: {
+    Footer
+  }
+}
+</script>
+
 
 <style>
 #app {
---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="ui one column centered grid container">
+      <div class="column">
+        <div class="ui center aligned basic segment">
+          <h4>Made with <span style="color: #e25555;">&hearts;</span> in San Francisco.</h4>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
