8c5de25d0e91c362307899e40349e545f5ddaa6a
Max Yi-Hsun Chou <maxchou415@gmail.com>
2019-03-02 08:28:00 +0800

    - Remove unused page and fix typo

--- src/components/Index/SectionAbout.vue
+++ src/components/Index/SectionAbout.vue
@@ -6,7 +6,7 @@
       </h1>
       <div class="ui segment">
         <p style="font-size: 1.05em;">
-          Max is a Software Engineer (Mainly focus on Javascript/Full-stack) who has 3 years developing experience. Good at communication. Outstanding leadership. Self-motivated fast learner.
+          Max is a Software Engineer (Mainly focuses on Javascript/Full-stack) who has 3 years developing experience. Good at communication. Outstanding leadership. Self-motivated fast learner.
           <br />
           <br />
           He has multiple startup experiences, one in the middle of 2016, the other mid-2017. Enrolled in the Y-Combinator Startup School in Sept. 2016.
